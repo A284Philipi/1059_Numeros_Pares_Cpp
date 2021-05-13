1d01028a3e17f43965977dff8bf890f7fa92791c
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-13 20:31:09 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int numero=2, cont=0;
+	while (cont < 100){
+		cout << numero <<endl;
+		numero = numero + 2;
+		cont = cont + 2;
+	}
+	return 0;
+}
